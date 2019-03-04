2f6dc686e9fd971068b45bde086d9ee68786b582
AxonDragonScale <r.r.n.harkhani@gmail.com>
2019-03-05 01:53:57 +0530

    Added HOST and JOIN buttons to MainActivity.
    Need to make intents in the onClickListeners.

--- app/src/main/java/com/sdpd/syncplayer/MainActivity.java
+++ app/src/main/java/com/sdpd/syncplayer/MainActivity.java
@@ -2,12 +2,35 @@ package com.sdpd.syncplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnHost;
+    Button btnJoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup the buttons and their on-click listeners
+        btnHost = findViewById(R.id.btn_host);
+        btnJoin = findViewById(R.id.btn_join);
+
+        btnHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
