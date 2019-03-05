7704b6f347e1558d21305269a5cec843c2580abf
AxonDragonScale <r.r.n.harkhani@gmail.com>
2019-03-05 14:18:02 +0530

    Added First time startup user registration.
    Added a SignUpActivity that asks the user for a Nick if the app is being
    started for the first time. Nick stored in sharedPrefs.

--- app/src/main/java/com/sdpd/syncplayer/MainActivity.java
+++ app/src/main/java/com/sdpd/syncplayer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sdpd.syncplayer;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // First Time Launch
+        SharedPreferences sharedPrefs = getSharedPreferences(getString(R.string.sharedPrefsKey), MODE_PRIVATE);
+        boolean isFirstRun = sharedPrefs.getBoolean(getString(R.string.isFirstRunSharedPrefsKey),true);
+        if(isFirstRun) {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(intent);
+        }
+
         // Setup the buttons and their on-click listeners
         btnHost = findViewById(R.id.btn_host);
         btnJoin = findViewById(R.id.btn_join);
@@ -22,14 +33,16 @@ public class MainActivity extends AppCompatActivity {
         btnHost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, HostActivity.class);
+                startActivity(intent);
             }
         });
 
         btnJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, ClientActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/sdpd/syncplayer/SignUpActivity.java
@@ -0,0 +1,44 @@
+package com.sdpd.syncplayer;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    Button btnSubmit;
+    TextInputLayout tilNickWrapper;
+    TextInputEditText tietNick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        tilNickWrapper = findViewById(R.id.til_nickWrapper);
+        tietNick = findViewById(R.id.tiet_nick);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nick = tietNick.getText().toString();
+
+                SharedPreferences sharedPrefs = getSharedPreferences(getString(R.string.sharedPrefsKey), MODE_PRIVATE);
+                SharedPreferences.Editor sharedPrefsEditor= sharedPrefs.edit();
+
+                sharedPrefsEditor.putString(getString(R.string.nickSharedPrefsKey), nick);
+                sharedPrefsEditor.putBoolean(getString(R.string.isFirstRunSharedPrefsKey), false);
+                sharedPrefsEditor.commit();
+
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
